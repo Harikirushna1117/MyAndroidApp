bbe0c7b0cd5880d86d65dc74847be716aa49e8b6
Harikirushna1117 <harikirushna1117@gmail.com>
2025-10-21 16:57:47 +0530

    Correct print statements in Test.java
    Fix syntax errors and improve print statements.

--- Test.java
+++ Test.java
@@ -1,1 +1,9 @@
+public class Test {
+  
+public static void main(String[] args) {
+System.out.println("************************米*来米米"）；
+System. out.printIn("Hello from GITHub");
+system.out.println（“**************来*****************"）；
+  }
+}
 
